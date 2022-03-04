8e9fb17be5082470516153e08fd8d3924cf8391e
Shantanu Soni <75296055+shantanusoni72@users.noreply.github.com>
2022-03-04 19:09:36 +0530

    Basic log system

--- server/components.py
+++ server/components.py
@@ -1,19 +1,29 @@
 from nltk import sentiment
 import nltk
 nltk.download('vader_lexicon')
-import random
 
 def sentiment_analysis(news,res):
 	emotions = sentiment.SentimentIntensityAnalyzer()
 	if news != '':
+		f = open('uses.log','a')
+		log_data = ''
 		status = emotions.polarity_scores(news)
 		if status['pos'] > status['neg']:
 			res["score"] = "Positive"
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			f.write(log_data)
+			f.close() 
 			return res
 		elif status['neg'] > status['pos']:
 			res["score"] = "Negative"
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			f.write(log_data)
+			f.close()
 			return res
 		else:
 			res["score"] = "Neutral"
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			f.write(log_data)
+			f.close()
 			return res
 		return res
