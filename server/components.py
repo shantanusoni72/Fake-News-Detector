2fa25f409ef9e60ccbbbe996519eacb1e789d0ee
Shantanu Soni <75296055+shantanusoni72@users.noreply.github.com>
2022-03-09 10:06:51 +0530

    Add usage and feedback log

--- server/components.py
+++ server/components.py
@@ -1,6 +1,7 @@
 from nltk import sentiment
 import nltk
 nltk.download('vader_lexicon')
+import datetime
 
 def sentiment_analysis(news,res):
 	emotions = sentiment.SentimentIntensityAnalyzer()
@@ -10,20 +11,26 @@ def sentiment_analysis(news,res):
 		status = emotions.polarity_scores(news)
 		if status['pos'] > status['neg']:
 			res["score"] = "Positive"
-			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +']' + '[' + str(datetime.datetime.now()) + ']' + '\n'
 			f.write(log_data)
 			f.close() 
 			return res
 		elif status['neg'] > status['pos']:
 			res["score"] = "Negative"
-			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +']' + str(datetime.datetime.now()) + '\n'
 			f.write(log_data)
 			f.close()
 			return res
 		else:
 			res["score"] = "Neutral"
-			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +'] \n'
+			log_data = '['+ str(res['id']) +'] ' + 'Message="'+ res['news_headline'] + '"' + '['+ str(res['score']) +']' + str(datetime.datetime.now()) + '\n'
 			f.write(log_data)
 			f.close()
 			return res
 		return res
+
+def write(res,loc):
+	f = open(loc,'a')
+	content = '['+ str(res['id']) +'] ' + 'Name="'+ res['name'] + '" ' + 'Email= "' + str(res['email']) + '" ' + 'Message=" ' + res['message'] + '" ' + '[' + str(datetime.datetime.now()) + ']' + '\n'
+	f.write(content)
+	f.close()
--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from nltk import sentiment
 import nltk
 nltk.download('vader_lexicon')
 from components import sentiment_analysis
+from components import write
 
 app = Flask(__name__)
 
@@ -17,6 +18,12 @@ def search(news):
    res = { "id": random.randint(0,100000), "news_headline":news ,"score": "","result":"" }
    news = sentiment_analysis(news,res)
    return news
+
+@app.route('/feedback/<string:email>/<string:name>/<string:message>')
+def feedback(email,name,message):
+    res = { "id": random.randint(0,100000), "email":email ,"name": name,"message":message }
+    write(res,'feedback.log')
+    return res
 
 if __name__ == "__main__":
    app.run(debug=True)
